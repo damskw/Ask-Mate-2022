a40fc2609a0baaa6d275886c8182092fe6fdba55
damskw <damian.skwierawski@gmail.com>
2022-09-06 00:26:52 +0200

    bug fixes

--- data_handler.py
+++ data_handler.py
@@ -166,12 +166,14 @@ def change_answer_vote(answer_id, direction):
                 if answer['id'] == answer_id:
                     vote_number = int(answer['vote number']) + 1
                     answer['vote number'] = vote_number
-                    file.write(f"{answer['id']},"
-                               f"{answer['submission time']},"
-                               f"{answer['vote number']},"
-                               f"{answer['question id']},"
-                               f"{answer['message']},"
-                               f"{answer['image']}\n")
+                answer['message'] = '"' + answer['message'] + '"'
+                answer['image'] = '"' + answer['image'] + '"'
+                file.write(f"{answer['id']},"
+                           f"{answer['submission time']},"
+                           f"{answer['vote number']},"
+                           f"{answer['question id']},"
+                           f"{answer['message']},"
+                           f"{answer['image']}\n")
     if direction == "DOWN":
         with open(ANSWERS_FILE_PATH, "w") as file:
             file.write("id,submission time,vote number,question id,message,image\n")
