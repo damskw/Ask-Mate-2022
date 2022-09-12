c81fea80489c4dd1965af2ba757efcf5f38bd27b
damskw <damian.skwierawski@gmail.com>
2022-09-12 14:41:49 +0200

    Update: * Whole project works with database now * Layout changes

--- data_handler/data_manager.py
+++ data_handler/data_manager.py
@@ -1,19 +1,158 @@
 from typing import List, Dict
-
+import config
 from psycopg2 import sql
 from psycopg2.extras import RealDictCursor
 
-import database_connection
+from data_handler import database_connection
 
 
-# @database_connection.connection_handler
-# def get_mentors(cursor):
-#     query = """
-#         SELECT first_name, last_name, city
-#         FROM mentor
-#         ORDER BY first_name"""
-#     cursor.execute(query)
-#     return cursor.fetchall()
+@database_connection.connection_handler
+def get_questions(cursor):
+    query = """
+        SELECT *
+        FROM question
+        ORDER BY id"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
+
+@database_connection.connection_handler
+def add_question(cursor, submission_time, title, message, image):
+    query = """
+        INSERT INTO question(submission_time, view_number, vote_number, title, message, image)
+        VALUES (%(submission_time)s, 0, 0, %(title)s, %(message)s, %(image)s)
+        """
+    cursor.execute(query, {"submission_time": submission_time, "title": title, "message": message, "image": image})
+
+
+@database_connection.connection_handler
+def find_question(cursor, question_id):
+    query = """
+        SELECT *
+        FROM question
+        WHERE id=%(question_id)s"""
+    cursor.execute(query, {"question_id": question_id})
+    return cursor.fetchone()
+
+
+@database_connection.connection_handler
+def find_answer_to_question(cursor, question_id):
+    query = """
+            SELECT * FROM answer
+            WHERE question_id = %(question_id)s
+            """
+    cursor.execute(query, {"question_id": question_id})
+    return cursor.fetchall()
+
+
+@database_connection.connection_handler
+def add_answer(cursor, submission_time, question_id, message, image):
+    query = """
+        INSERT INTO answer(submission_time, vote_number, question_id, message, image)
+        VALUES (%(submission_time)s, 0, %(question_id)s, %(message)s, %(image)s)
+        """
+    cursor.execute(query,
+                   {"submission_time": submission_time, "question_id": question_id, "message": message, "image": image})
+
+
+@database_connection.connection_handler
+def delete_question(cursor, question_id):
+    query = """
+        DELETE from answer
+        WHERE question_id=%(question_id)s;
+        
+        DELETE from question
+        WHERE id=%(question_id)s;"""
+    cursor.execute(query, {"question_id": question_id})
+
+
+@database_connection.connection_handler
+def delete_answer(cursor, answer_id):
+    query = """
+        DELETE from answer
+        WHERE id=%(answer_id)s"""
+    cursor.execute(query, {"answer_id": answer_id})
+
+
+@database_connection.connection_handler
+def find_question_id(cursor, answer_id):
+    query = """
+            SELECT question_id FROM answer
+            WHERE id = %(answer_id)s
+            """
+    cursor.execute(query, {"answer_id": answer_id})
+    return cursor.fetchone()
+
+
+@database_connection.connection_handler
+def get_question_image_name(cursor, question_id):
+    query = """
+            SELECT image FROM question
+            WHERE id = %(question_id)s
+            """
+    cursor.execute(query, {"question_id": question_id})
+    return cursor.fetchone()
+
+
+@database_connection.connection_handler
+def get_answer_image_name(cursor, answer_id):
+    query = """
+            SELECT image FROM answer
+            WHERE id = %(answer_id)s
+            """
+    cursor.execute(query, {"answer_id": answer_id})
+    return cursor.fetchone()
+
+
+@database_connection.connection_handler
+def update_question(cursor, question_id, title, message, image):
+    query = """
+        UPDATE question
+        SET title = %(title)s, message = %(message)s, image = %(image)s
+        WHERE id=%(question_id)s"""
+    cursor.execute(query, {"question_id": question_id, "title": title, "message": message, "image": image})
+
+
+@database_connection.connection_handler
+def update_question_vote(cursor, question_id, direction):
+    if direction == config.UP:
+        query = """
+            UPDATE question
+            SET vote_number = vote_number + 1
+            WHERE id=%(question_id)s"""
+        cursor.execute(query, {"question_id": question_id})
+    elif direction == config.DOWN:
+        query = """
+            UPDATE question
+            SET vote_number = vote_number - 1
+            WHERE id=%(question_id)s"""
+        cursor.execute(query, {"question_id": question_id})
+
+
+@database_connection.connection_handler
+def update_answer_vote(cursor, answer_id, direction):
+    if direction == config.UP:
+        query = """
+            UPDATE answer
+            SET vote_number = vote_number + 1
+            WHERE id=%(answer_id)s"""
+        cursor.execute(query, {"answer_id": answer_id})
+    elif direction == config.DOWN:
+        query = """
+            UPDATE answer
+            SET vote_number = vote_number - 1
+            WHERE id=%(answer_id)s"""
+        cursor.execute(query, {"answer_id": answer_id})
+
+
+@database_connection.connection_handler
+def increase_question_view_number(cursor, question_id):
+    query = """
+        UPDATE question
+        SET view_number = view_number + 1
+        WHERE id=%(question_id)s"""
+    cursor.execute(query, {"question_id": question_id})
+
 #
 #
 # @database_connection.connection_handler
--- server.py
+++ server.py
@@ -3,10 +3,10 @@ from datetime import datetime
 from werkzeug.utils import secure_filename
 import config
 import os
-from data_handler import data_handler, id_generator
+from data_handler import data_handler, id_generator, data_manager
 from utils import quote, convert_timestamp_to_utc
 
-UPLOAD_FOLDER = 'static'
+UPLOAD_FOLDER = 'static/images'
 app = Flask(__name__, static_url_path='/static')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -14,61 +14,33 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 @app.route("/")
 @app.route("/list")
 def list_questions():
-    order_by = request.args.get("order_by", "title")
+    order_by = request.args.get("order_by", "submission_time")
     order_direction = request.args.get("order_direction", "desc")
-    questions = data_handler.get_questions()
-    for question in questions:
-        try:
-            question[config.SUBMISSION_TIME] = convert_timestamp_to_utc(question.get(config.SUBMISSION_TIME))
-        except ValueError:
-            question[config.SUBMISSION_TIME] = ""
+    questions = data_manager.get_questions()
     questions.sort(key=lambda q: q[order_by], reverse=(order_direction == "desc"))
     return render_template('list.html', questions=questions)
 
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
-
-
 @app.route("/question/<question_id>", methods=[config.GET, config.POST])
 def display_question(question_id):
-    data_handler.increment_question_view_number(question_id)
-    question = data_handler.get_question(question_id)
-    try:
-        question[config.SUBMISSION_TIME] = convert_timestamp_to_utc(question.get(config.SUBMISSION_TIME))
-    except ValueError:
-        question[config.SUBMISSION_TIME] = ""
-    desired_answers = data_handler.get_answers_to_question(question_id)
-    if desired_answers is not None:
-        for answer in desired_answers:
-            try:
-                answer[config.SUBMISSION_TIME] = convert_timestamp_to_utc(answer.get(config.SUBMISSION_TIME))
-            except ValueError:
-                answer[config.SUBMISSION_TIME] = ""
-    return render_template('display_question.html', question=question,
-                           desired_answers=desired_answers)
+    question = data_manager.find_question(question_id)
+    answers = data_manager.find_answer_to_question(question_id)
+    data_manager.increase_question_view_number(question_id)
+    if question:
+        return render_template('display_question.html', question=question, answers=answers)
+    return render_template('404.html')
 
 
 @app.route("/add-question", methods=[config.GET, config.POST])
 def add_question():
     if request.method == config.POST:
-        title = quote(request.form[config.TITLE])
-        question = quote(request.form[config.QUESTION])
-        question_ids = list(map(lambda question: question[config.ID], data_handler.get_questions()))
-        question_id = id_generator.generate_unique_id(question_ids, 3, 3, 3, 3)
+        title = request.form[config.TITLE]
+        question = request.form[config.QUESTION]
         now = datetime.now()
         timestamp = int(datetime.timestamp(now))
         img_filename = save_image_file(request, timestamp)
-        data_handler.save_question({config.ID: question_id,
-                                    config.SUBMISSION_TIME: timestamp,
-                                    config.VIEW_NUMBER: 0,
-                                    config.VOTE_NUMBER: 0,
-                                    config.TITLE: title,
-                                    config.MESSAGE: question,
-                                    config.IMAGE: img_filename
-                                    })
-        return redirect(f'/question/{question_id}')
+        data_manager.add_question(now, title, question, img_filename)
+        return redirect('/')
     return render_template('add_question.html')
 
 
@@ -89,61 +61,34 @@ def save_image_file(request, timestamp: int = None) -> str:
 def add_answer(question_id):
     if request.method == config.GET:
         return render_template('add_answer.html', question_id=question_id)
-    
-    answer = quote(request.form[config.ANSWER])
-    answer_ids = list(map(lambda question: question[config.ID], data_handler.get_questions()))
-    answer_id = id_generator.generate_unique_id(answer_ids, 2, 2, 2, 2)
+    answer = request.form[config.ANSWER]
     now = datetime.now()
     timestamp = int(datetime.timestamp(now))
     img_filename = save_image_file(request, timestamp)
-    data_handler.save_answer({config.ID: answer_id,
-                                config.SUBMISSION_TIME: timestamp,
-                                config.VOTE_NUMBER: 0,
-                                config.QUESTION_ID: question_id,
-                                config.MESSAGE: answer,
-                                config.IMAGE: img_filename,
-                                })
+    data_manager.add_answer(now, question_id, answer, img_filename)
     return redirect(f'/question/{question_id}')
 
 
 @app.route("/question/<question_id>/delete", methods=[config.GET, config.POST])
 def delete_question(question_id):
-    all_questions = data_handler.get_questions()
-    try:
-        question_to_delete = next(filter(lambda question: question[config.ID] == question_id, all_questions))
-    except StopIteration:
-        return redirect('/404')
-    image_name = question_to_delete[config.IMAGE]
-    if image_name != "" and os.path.exists(f"static/{image_name}"):
-        os.remove(f"static/{image_name}")
-    all_questions.remove(question_to_delete)
-    all_answers = data_handler.get_answers()
-    answers_to_this_question = data_handler.get_answers_to_question(question_id)
-    for answer in answers_to_this_question:
-        all_answers.remove(answer)
-    data_handler.update_answers(all_answers)
-    data_handler.update_questions(all_questions)
-
+    question = data_manager.get_question_image_name(question_id)
+    image_name = question[config.IMAGE]
+    data_manager.delete_question(question_id)
+    if image_name != "" and os.path.exists(f"static/images/{image_name}"):
+        os.remove(f"static/images/{image_name}")
     return redirect('/list')
 
 
 @app.route("/answer/<answer_id>/delete", methods=[config.GET, config.POST])
 def delete_answer(answer_id):
-    all_answers = data_handler.get_answers()
-    try:
-        answer = next(filter(lambda answer: answer[config.ID] == answer_id, all_answers))
-    except StopIteration:
-        return redirect("/404")
-    image_name = answer[config.IMAGE]
-    if image_name != "" and os.path.exists(f"static/{image_name}"):
-        os.remove(f"static/{image_name}")
-    question_id = answer[config.QUESTION_ID]
-    question = data_handler.get_question(question_id)
-    all_answers.remove(answer)
-    data_handler.update_answers(all_answers)
-    
-    result = f'/question/{question_id}' if question is not None else '/'
-    return redirect(result)
+    question = data_manager.find_question_id(answer_id)
+    question_id = question[config.QUESTION_ID]
+    question = data_manager.get_answer_image_name(answer_id)
+    image_name = question[config.IMAGE]
+    data_manager.delete_answer(answer_id)
+    if image_name != "" and os.path.exists(f"static/images/{image_name}"):
+        os.remove(f"static/images/{image_name}")
+    return redirect(f'/question/{question_id}')
 
 
 @app.route("/question/<question_id>/edit", methods=[config.GET, config.POST])
@@ -152,19 +97,15 @@ def edit_question(question_id):
         title = request.form[config.TITLE]
         question = request.form[config.QUESTION]
         img_filename = save_image_file(request)
-        data_handler.edit_question({config.ID: question_id,
-                                    config.TITLE: title,
-                                    config.MESSAGE: question,
-                                    config.IMAGE: img_filename
-                                    })
+        data_manager.update_question(question_id, title, question, img_filename)
         return redirect(f'/question/{question_id}')
-    question_to_be_edited = data_handler.get_question(question_id)
+    question_to_be_edited = data_manager.find_question(question_id)
     return render_template('edit_question.html', question=question_to_be_edited)
 
 
 @app.route("/question/<question_id>/vote-up", methods=[config.GET, config.POST])
 def vote_question_up(question_id):
-    data_handler.change_question_vote(question_id, config.UP)
+    data_manager.update_question_vote(question_id, config.UP)
     # TODO (in the farther futures): do not redirect, only send a request with JS and update value of DOM based on
     #  response
     return redirect(f'/question/{question_id}')
@@ -172,7 +113,7 @@ def vote_question_up(question_id):
 
 @app.route("/question/<question_id>/vote-down", methods=[config.GET, config.POST])
 def vote_question_down(question_id):
-    data_handler.change_question_vote(question_id, config.DOWN)
+    data_manager.update_question_vote(question_id, config.DOWN)
     # TODO (in the farther futures): do not redirect, only send a request with JS and update value of DOM based on
     #  response
     return redirect(f'/question/{question_id}')
@@ -180,17 +121,17 @@ def vote_question_down(question_id):
 
 @app.route("/answer/<answer_id>/vote-up", methods=[config.GET, config.POST])
 def vote_answer_up(answer_id):
-    answer = data_handler.get_answer(answer_id)
-    question_id = answer[config.QUESTION_ID]
-    data_handler.change_answer_vote(answer_id, config.UP)
+    data_manager.update_answer_vote(answer_id, config.UP)
+    question = data_manager.find_question_id(answer_id)
+    question_id = question[config.QUESTION_ID]
     return redirect(f'/question/{question_id}')
 
 
 @app.route("/answer/<answer_id>/vote-down", methods=[config.GET, config.POST])
 def vote_answer_down(answer_id):
-    answer = data_handler.get_answer(answer_id)
-    question_id = answer[config.QUESTION_ID]
-    data_handler.change_answer_vote(answer_id, config.DOWN)
+    data_manager.update_answer_vote(answer_id, config.DOWN)
+    question = data_manager.find_question_id(answer_id)
+    question_id = question[config.QUESTION_ID]
     return redirect(f'/question/{question_id}')
 
 
